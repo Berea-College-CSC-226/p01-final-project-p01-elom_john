0459d1a5162ab4ea1d9747a8766f6280b5f64d19
lolongaj <lolongaj@berea.edu>
2026-04-22 21:13:56 -0400

    created the road environment and placed the car item(image) on the screen

--- original_code/Game.py
+++ original_code/Game.py
@@ -1,17 +1,28 @@
 import pygame
+from playercar import PlayerCar
 
 
 class Game:
     def __init__(self):
         pygame.init()
 
-        self.width = 700
+        self.width = 600
         self.height = 700
         self.screen = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption("Car Dodge Game")
 
         self.clock = pygame.time.Clock()
         self.running = True
+
+        road_width = 300
+        road_x = (self.width - road_width) // 2
+        lane_width = road_width // 3
+
+        # place car in middle lane
+        car_x = road_x + lane_width + (lane_width - 50) // 2
+        car_y = self.height - 120
+
+        self.player = PlayerCar(car_x, car_y)
 
     def draw_road(self):
         road_width = 300
@@ -20,27 +31,39 @@ class Game:
         # draw road
         pygame.draw.rect(self.screen, (50, 50, 50), (road_x, 0, road_width, self.height))
 
-        # lane lines
+        # lane settings
         lane_color = (255, 255, 255)
         lane_width = 5
         dash_height = 20
         gap = 20
 
-        middle_x = self.width // 2
+        # 3 lanes means 2 divider lines
+        lane_size = road_width // 3
+        line1_x = road_x + lane_size
+        line2_x = road_x + 2 * lane_size
 
         for y in range(0, self.height, dash_height + gap):
             pygame.draw.rect(
                 self.screen,
                 lane_color,
-                (middle_x - lane_width // 2, y, lane_width, dash_height)
+                (line1_x - lane_width // 2, y, lane_width, dash_height)
+            )
+            pygame.draw.rect(
+                self.screen,
+                lane_color,
+                (line2_x - lane_width // 2, y, lane_width, dash_height)
             )
 
+
+
     def draw(self):
-        # background (grass)
+        # background (grass color)
         self.screen.fill((34, 139, 34))
 
         # draw road
         self.draw_road()
+        #Draw car
+        self.player.draw(self.screen)
 
         pygame.display.update()
 
--- original_code/playercar.py
+++ original_code/playercar.py
@@ -1,13 +1,23 @@
 import pygame
 
+
 class PlayerCar:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.width = 50
+        self.height = 90
+        self.speed = 6
 
-        # load your image
-        self.image = pygame.image.load("image/car_image.png")
-        self.image = pygame.transform.scale(self.image, (50, 100))
+        # correct path to image
+        self.image = pygame.image.load("../image/car_image.png")
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+
+    def move_left(self):
+        self.x -= self.speed
+
+    def move_right(self):
+        self.x += self.speed
 
     def draw(self, screen):
         screen.blit(self.image, (self.x, self.y))
